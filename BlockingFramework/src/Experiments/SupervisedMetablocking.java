b8dacfa1c3604a1a39a71f9f8bd17044b938e9a5
guilherme <dbguilherme@gmail.com>
2016-05-04 18:32:48 -0300

    alteração na entrada dos dados e na funçao de contagem

--- BlockingFramework/src/Experiments/SupervisedMetablocking.java
+++ BlockingFramework/src/Experiments/SupervisedMetablocking.java
@@ -25,6 +25,8 @@ import DataStructures.IdDuplicates;
 import SupervisedMetablocking.SupervisedCNP;
 import SupervisedMetablocking.SupervisedWEP;
 import Utilities.BlockStatistics;
+import Utilities.ComparisonIterator;
+import Utilities.Converter;
 import Utilities.ExecuteBlockComparisons;
 import Utilities.ExportBlocks;
 import Utilities.SerializationUtilities;
@@ -33,6 +35,7 @@ import libsvm.svm;
 import libsvm.svm_model;
 import libsvm.svm_parameter;
 import libsvm.svm_problem;
+import sun.font.StrikeCache;
 import weka.classifiers.Classifier;
 import weka.classifiers.bayes.BayesNet;
 import weka.classifiers.bayes.NaiveBayes;
@@ -172,54 +175,102 @@ public class SupervisedMetablocking {
 		return null;
 	}
 
+	
+	private static int[] conta_niveis_hash(List<AbstractBlock> blocks, ExecuteBlockComparisons ebc) {
+
+		int[] blockSize=  new int[10];
+		for (int i = 0; i < 10; i++) {
+			blockSize[i]=0;
+			//	primeiroBlock[i]=0;
+		}
+		EntityIndex entityIndex = new EntityIndex(blocks);
+		for ( AbstractBlock b:blocks) {
+			if(b!=null){
+				ComparisonIterator comparisonit = b.getComparisonIterator();
+				b.getBlockIndex();
+				while(comparisonit.hasNext()){
+					DataStructures.Comparison c=comparisonit.next();
+
+					final List<Integer> commonBlockIndices = entityIndex.getCommonBlockIndices(b.getBlockIndex(), c);
+					if (commonBlockIndices == null) {
+						continue;
+					}
+
+					Double sim=ebc.getSImilarityAttribute(c.getEntityId1(),c.getEntityId2(),Converter.atributos_value);
+					c.sim=sim;
+					if(sim==10)
+						sim--;
+					blockSize[((int)Math.floor(sim*10))]++;
+				}
+			}
+		}
+		for (int i = 0; i < 10; i++) {
+			//	perc[i]=(((double)tamanho)/(blockSize[i]));
+			System.out.println(i + " tamanho do bloco "+  "  " + blockSize[i] );
+			//totalPares += blockHash.blockSize[i];
+		}
+		return blockSize;
+	}
+	
 	public static void main(String[] args) throws IOException, Exception {
 		//experiments with synthetic datasets
 		//the blocks are constructed on the fly
+	
+		
+		
 		System.out.println( System.getProperty("user.home"));
+		String mainDirectory;
+		String profilesPathA=null;
+		String profilesPathB=null;
+		String groundTruthPath = null;
+		switch(args[0]){
+		case "1":	       
+			mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/sintetica";
+			profilesPathA =  mainDirectory+"/"+args[1]+"profiles"	;	
+			groundTruthPath =  mainDirectory+"/"+args[1]+"IdDuplicates";			
+			
+			break;
+		case "2":
+			mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/base_clean_serializada";
+			profilesPathA= mainDirectory+"/dblp";
+			profilesPathB= mainDirectory+"/scholar";
+			groundTruthPath =  mainDirectory+ "/groundtruth"; 
+			break;
+		case "3":
+			mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/movies";
+			profilesPathA= mainDirectory+"/dataset1_imdb";
+			profilesPathB= mainDirectory+"/dataset2_dbpedia";
+			groundTruthPath =  mainDirectory+ "/groundtruth"; 
+			break;
+//		case 1:
+//			break;
+//		case 1:
+//			break;
+			
+		}
 		///home/guilhermedb/Dropbox/blocagem/bases/base_clean_serializada
-		//		String mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/base_clean_serializada";
-		//		//String mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/movies";
-		//		String[] profilesPath = { 
-		//				mainDirectory+"/dblp",
-		//				mainDirectory+"/scholar"};
-		//		//mainDirectory+"/dataset1_imdb",
-		//	//	mainDirectory+"/dataset1_dbpedia"};
-		//
-		//		String[] groundTruthPath = { mainDirectory+ "/groundtruth"};  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//				String mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/base_clean_serializada";
+//				//String mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/movies";
+//				String[] profilesPath = { 
+//						mainDirectory+"/dblp",
+//						mainDirectory+"/scholar"};
+//			//	mainDirectory+"/dataset1_imdb",
+//		//	mainDirectory+"/dataset2_dbpedia"};
+//		
+//				String[] groundTruthPath = { mainDirectory+ "/groundtruth"};  
 
 		//	
-		String mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/sintetica";
-		String[] profilesPath = {   
-				mainDirectory+"/50Kprofiles"
-
-		};
-
-		String[] groundTruthPath = {   
-
-				mainDirectory+"/50KIdDuplicates"
-
-		};
+//		String mainDirectory = System.getProperty("user.home")+"/Dropbox/blocagem/bases/sintetica";
+//		String[] profilesPath = {   
+//				mainDirectory+"/"+args[0]+"profiles"
+//
+//		};
+//
+//		String[] groundTruthPath = {   
+//
+//				mainDirectory+"/"+args[0]+"IdDuplicates"
+//
+//		};
 
 
 
@@ -254,46 +305,58 @@ public class SupervisedMetablocking {
 		//     for (int i = 0; i < profilesPath.length; i++) {
 		//    System.out.println("\n\n\n\n\nCurrent dataset\t:\t" + profilesPath[i]);
 
-		Set<IdDuplicates> duplicatePairs = (HashSet<IdDuplicates>) SerializationUtilities.loadSerializedObject(groundTruthPath[0]);
+		Set<IdDuplicates> duplicatePairs = (HashSet<IdDuplicates>) SerializationUtilities.loadSerializedObject(groundTruthPath);
 		System.out.println("Existing duplicates\t:\t" + duplicatePairs.size());
 
 
 
-		//
+		List<AbstractBlock> blocks;
 		List<EntityProfile>[] profiles ;
-		//		if(profilesPath.length>1){
-		//			profiles = new List[2];
-		//			profiles[0] = (List<EntityProfile>) SerializationUtilities.loadSerializedObject(profilesPath[0]);
-		//			profiles[1] = (List<EntityProfile>) SerializationUtilities.loadSerializedObject(profilesPath[1]);
-		//		}else
-		{
+		if(profilesPathB != null){
+					profiles = new List[2];
+					profiles[0] = (List<EntityProfile>) SerializationUtilities.loadSerializedObject(profilesPathA);
+					profiles[1] = (List<EntityProfile>) SerializationUtilities.loadSerializedObject(profilesPathB);
+					TokenBlocking imtb = new TokenBlocking(profiles);
+					 blocks = imtb.buildBlocks();
+					SizeBasedBlockPurging sbb= new SizeBasedBlockPurging();
+					sbb.applyProcessing(blocks);
+		}else	{
 			profiles= new List[1];
-			profiles[0] = (List<EntityProfile>) SerializationUtilities.loadSerializedObject(profilesPath[0]);
+			profiles[0] = (List<EntityProfile>) SerializationUtilities.loadSerializedObject(profilesPathA);
+			TokenBlocking imtb = new TokenBlocking(profiles);
+			 blocks = imtb.buildBlocks();
+			AbstractEfficiencyMethod blockPurging = new ComparisonsBasedBlockPurging(1.005);
+			blockPurging.applyProcessing(blocks);
 		}
 
 
 		//		System.out.println(" database  1  "+ profiles[0].size()  +"  data 2 ->" + profiles[1].size());
-		TokenBlocking imtb = new TokenBlocking(profiles);
-		List<AbstractBlock> blocks = imtb.buildBlocks();
+	
+		String[] profilesPath;
+		if(profilesPathB!=null){
+			 profilesPath=new String[2];
+			 profilesPath[0]=profilesPathA;
+			 profilesPath[1]=profilesPathB;
+		}else{
+			 profilesPath=new String[1];
+			 profilesPath[0]=profilesPathA;
+		}
+		//
 
-		//SizeBasedBlockPurging sbb= new SizeBasedBlockPurging();
-		//sbb.applyProcessing(blocks);
+		int num_blocks=0;
+		for ( AbstractBlock b:blocks) {
+			if(b!=null){
+				num_blocks+=b.getNoOfComparisons();
 
-		AbstractEfficiencyMethod blockPurging = new ComparisonsBasedBlockPurging(1.005);
-		blockPurging.applyProcessing(blocks);
+			}
+		}   
+		System.out.println(" numero comparações --> "+ num_blocks);
 
-//		int num_blocks=0;
-//		for ( AbstractBlock b:blocks) {
-//			if(b!=null){
-//				num_blocks+=b.getNoOfComparisons();
-//
-//			}
-//		}   
-//		System.out.println(" blocks --> "+ num_blocks);
-
+		
 		ExecuteBlockComparisons ebc = new ExecuteBlockComparisons(profilesPath);
 
-		//            System.out.println("\n\n\n\n\n======================= Supervised CEP =======================");
+		int Nblocks[]=conta_niveis_hash(blocks,ebc);
+		System.out.println("\n\n\n\n\n======================= Supervised CEP =======================");
 		Classifier[] classifiers = getSupervisedCepClassifiers();
 		//            SupervisedCEP scep = new SupervisedCEP(classifiers.length, blocks, duplicatePairs);
 		//            for (int j = 0; j < ITERATIONS; j++) {
@@ -331,7 +394,7 @@ public class SupervisedMetablocking {
 			swep = new SupervisedWEP(classifiers.length, blocks, duplicatePairs,ebc);
 			//blockHash.produceHash(blocks, ebc);
 
-			int tamanho = 50;
+			int tamanho = 10;
 
 			while(tamanho <=1000)
 			{
@@ -341,7 +404,7 @@ public class SupervisedMetablocking {
 				writer3.write("level "+tamanho +"\n");
 				writer4.write("level "+tamanho +"\n");
 				for (int j = 0; j < 10; j++) {
-					swep.applyProcessing(j, classifiers, ebc, tamanho, writer1,writer2,writer3,writer4,i);
+					swep.applyProcessing(j, classifiers, ebc, tamanho, writer1,writer2,writer3,writer4,i, Nblocks);
 					writer1.flush();
 					writer2.flush();
 					writer3.flush();
@@ -352,15 +415,6 @@ public class SupervisedMetablocking {
 				//swep.printStatisticsB(writer);
 				System.out.println("size of level : "+ tamanho);
 
-	/*			if(tamanho==5)
-=======
-		/*		if(tamanho==5)
->>>>>>> 5daee64caf8372bbedaf6014f6bc6e66f2535586
-					tamanho+=5;
-				else if(tamanho==10)
-					tamanho+=10;
-				else
-					tamanho+=20;*/
 				if(tamanho==5)
 					tamanho=10;
 				else if(tamanho==10)
